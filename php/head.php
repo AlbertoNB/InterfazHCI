273755fe0b8e638ef420dfaa9ae098bb799ea71d
Alberto Naula Bone <anaula8277@utm.edu.ec>
2021-07-04 19:49:52 -0500

    nuevos colores de text a la class "navbar-light"

--- php/head.php
+++ php/head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../scss/utm_bootstrap.css"><!--../css/bootstrap.min.css-->
     <link rel="stylesheet" href="../css2/cambios.1.css">
     <link rel="stylesheet" href="../css2/detalles.css">
     <title>index</title>

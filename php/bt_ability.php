95b52dda34694d51f297fe22886650441dbdd44d
Alberto Naula Bone <anaula8277@utm.edu.ec>
2021-07-17 00:24:41 -0500

    problema anterior del desplazamiento del menu de aumentado de texto ya solucionado

--- php/bt_ability.php
+++ php/bt_ability.php
@@ -1,29 +1,74 @@
-<div class="dropdown fixed-bottom">
-    <button class="btn btn-sm btn-warning m-2" type="button" id="menuability" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<div class="fixed-bottom">
+    <div class="collapse mx-2" id="ability">
+        <div class="card card-body p-2" style="width: 12rem;">
+            <button type="button d-inline" class="aumentar btn btn-secondary btn-sm mb-1">
+                <svg class="px-0" width="25px" height="25px" fill="currentColor">
+                    <use xlink:href="../svg/bootstrap-icons.svg#fonts" />
+                </svg>
+                <svg class="px-0" width="25px" height="25px" fill="currentColor">
+                    <use xlink:href="../svg/bootstrap-icons.svg#plus" />
+                </svg>
+                Aumentar
+            </button>
+            <button type="button" class="disminuir btn btn-secondary btn-sm mb-1">
+                <svg class="px-0" width="25px" height="25px" fill="currentColor">
+                    <use xlink:href="../svg/bootstrap-icons.svg#fonts" />
+                </svg>
+                <svg class="px-0" width="25px" height="25px" fill="currentColor">
+                    <use xlink:href="../svg/bootstrap-icons.svg#dash" />
+                </svg>
+                Disminuir
+            </button>
+            <button type="button" class="restablecer btn btn-secondary btn-sm">
+                <svg class="px-0" width="25px" height="25px" fill="currentColor">
+                    <use xlink:href="../svg/bootstrap-icons.svg#fonts" />
+                </svg>
+                <svg class="px-0" style=" transform: rotate(-90deg); " width="25px" height="25px" fill="currentColor">
+                    <use xlink:href="../svg/bootstrap-icons.svg#pause" />
+                </svg>
+                Restablecer
+            </button>
+        </div>
+    </div>
+    <button class="btn btn-sm btn-warning m-2" type="button" data-toggle="collapse" data-target="#ability"
+        aria-expanded="false">
         <svg class="bi" width="30px" height="30px" fill="currentColor">
             <use xlink:href="../svg/bootstrap-icons.svg#info-square" />
         </svg>
     </button>
-    <div class="dropdown-menu text-muted d-flex justify-content-center" aria-labelledby="menuability">
-        <button type="button" class="aumentar btn btn-secondary btn-sm">
-            <svg class="bi" width="25px" height="25px" fill="currentColor">
-                <use xlink:href="../svg/bootstrap-icons.svg#plus" />
-            </svg>
-        </button>
-        <button type="button" class="disminuir btn btn-secondary btn-sm mx-1">
-            <svg class="bi" width="25px" height="25px" fill="currentColor">
-                <use xlink:href="../svg/bootstrap-icons.svg#dash" />
-            </svg>
-        </button>
-        <button type="button" class="restablecer btn btn-secondary btn-sm">
-            <svg class="bi" width="25px" height="25px" fill="currentColor">
-                <use xlink:href="../svg/bootstrap-icons.svg#view-stacked" />
-            </svg>
-        </button>
-    </div>
 </div>
-<!--<div class="btn-group dropup">
-  <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropup
-  </button>
-</div>-->
+
+<!-- el boton se desplaza a la derecha
+ <div class="fixed-bottom p-2">
+    <div class="row pl-3">
+        <div class="col-auto p-0">
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body" style="width: 12rem;">
+                    <button type="button" class="aumentar btn btn-secondary btn-sm">
+                        <svg class="bi" width="25px" height="25px" fill="currentColor">
+                            <use xlink:href="../svg/bootstrap-icons.svg#plus" />
+                        </svg>
+                    </button>
+                    <button type="button" class="disminuir btn btn-secondary btn-sm">
+                        <svg class="bi" width="25px" height="25px" fill="currentColor">
+                            <use xlink:href="../svg/bootstrap-icons.svg#dash" />
+                        </svg>
+                    </button>
+                    <button type="button" class="restablecer btn btn-secondary btn-sm">
+                        <svg class="bi" width="25px" height="25px" fill="currentColor">
+                            <use xlink:href="../svg/bootstrap-icons.svg#view-stacked" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col p-0 d-flex align-items-end">
+            <button class="btn btn-sm btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                <svg class="bi" width="30px" height="30px" fill="currentColor">
+                    <use xlink:href="../svg/bootstrap-icons.svg#info-square" />
+                </svg>
+            </button>
+        </div>
+    </div>
+ </div>
+-->

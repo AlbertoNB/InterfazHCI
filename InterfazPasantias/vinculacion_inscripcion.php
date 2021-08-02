44e6ab447bb06fe2a3dd9d93006b769dec9d37fc
Alberto Naula Bone <anaula8277@utm.edu.ec>
2021-08-02 00:35:13 -0500

    vinculacion_inscripcion.php completado

--- InterfazPasantias/vinculacion_inscripcion.php
+++ InterfazPasantias/vinculacion_inscripcion.php
@@ -21,8 +21,8 @@
             </div>
         </div>
     </div>
-    <ul class="list-group  ">
-        <li class="list-group-item py-1 ">
+    <ul class="list-group">
+        <li class="list-group-item py-1">
             <div class="row d-flex align-items-center">
                 <div class="col pr-1">
                     <h5 class="mb-1 font-weight-bold text-small">Incumplimiento del syllabusdiseño de un sistema
@@ -30,28 +30,31 @@
                     <p class="mb-1"><b>Codigo:</b> FCI-CISI-PROG003-PROY010-2020</p>
                     <button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
                         data-target="#target-convenio">Convenios</button>
-                    <button type="button" class="btn btn-info btn-sm">Más detalles</button>
+                    <button type="button" class="btn btn-info btn-sm" data-toggle="modal"
+                        data-target="#target-detalles">Más detalles</button>
                 </div>
                 <div class="col-auto px-1">
                     <button type="button" class="mb-1 btn btn-warning btn-sm">
                         <svg class="" width="20px" height="20px" fill="currentColor">
-                            <use xlink:href="../svg/bootstrap-icons.svg#eye-fill" />
+                            <use xlink:href="../svg/bootstrap-icons.svg#file-earmark-post" />
                         </svg>
                     </button> <br>
-                    <button type="button" class=" mb-1 btn btn-warning btn-sm">
-                        <svg class="" width="20px" height="20px" fill="currentColor">
-                            <use xlink:href="../svg/bootstrap-icons.svg#check" />
-                        </svg>
-                    </button> <br>
-                    <button type="button" class="btn btn-warning btn-sm">
+                    <button type="button" class=" mb-1 btn btn-warning btn-sm" data-toggle="modal"
+                        data-target="#target-atencion">
                         <svg class="" width="20px" height="20px" fill="currentColor">
                             <use xlink:href="../svg/bootstrap-icons.svg#info-circle" />
+                        </svg>
+                    </button> <br>
+                    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
+                    data-target="#target-inscripcion">
+                        <svg class="" width="20px" height="20px" fill="currentColor">
+                            <use xlink:href="../svg/bootstrap-icons.svg#check" />
                         </svg>
                     </button>
                 </div>
             </div>
         </li>
-        <li class="list-group-item py-1 ">
+        <li class="list-group-item py-1">
             <div class="row d-flex align-items-center">
                 <div class="col pr-1">
                     <h5 class="mb-1 font-weight-bold text-small">Encuesta de empleo portoviejo</h5>
@@ -62,17 +65,44 @@
                 <div class="col-auto px-1">
                     <button type="button" class="mb-1 btn btn-warning btn-sm">
                         <svg class="" width="20px" height="20px" fill="currentColor">
-                            <use xlink:href="../svg/bootstrap-icons.svg#eye-fill" />
+                            <use xlink:href="../svg/bootstrap-icons.svg#file-earmark-post" />
                         </svg>
                     </button> <br>
                     <button type="button" class=" mb-1 btn btn-warning btn-sm">
                         <svg class="" width="20px" height="20px" fill="currentColor">
-                            <use xlink:href="../svg/bootstrap-icons.svg#check" />
+                            <use xlink:href="../svg/bootstrap-icons.svg#info-circle" />
                         </svg>
                     </button> <br>
-                    <button type="button" class="btn btn-warning btn-sm">
+                    <button type="button" class="btn btn-primary btn-sm">
+                        <svg class="" width="20px" height="20px" fill="currentColor">
+                            <use xlink:href="../svg/bootstrap-icons.svg#check" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </li>
+        <li class="list-group-item py-1">
+            <div class="row d-flex align-items-center">
+                <div class="col pr-1">
+                    <h5 class="mb-1 font-weight-bold text-small">---------------------------------------</h5>
+                    <p class="mb-1"><b>Codigo:</b> FCI-CISI-----------------------</p>
+                    <button type="button" class="btn btn-primary btn-sm">Convenios</button>
+                    <button type="button" class="btn btn-info btn-sm">Más detalles</button>
+                </div>
+                <div class="col-auto px-1">
+                    <button type="button" class="mb-1 btn btn-warning btn-sm">
+                        <svg class="" width="20px" height="20px" fill="currentColor">
+                            <use xlink:href="../svg/bootstrap-icons.svg#file-earmark-post" />
+                        </svg>
+                    </button> <br>
+                    <button type="button" class=" mb-1 btn btn-warning btn-sm">
                         <svg class="" width="20px" height="20px" fill="currentColor">
                             <use xlink:href="../svg/bootstrap-icons.svg#info-circle" />
+                        </svg>
+                    </button> <br>
+                    <button type="button" class="btn btn-primary btn-sm">
+                        <svg class="" width="20px" height="20px" fill="currentColor">
+                            <use xlink:href="../svg/bootstrap-icons.svg#check" />
                         </svg>
                     </button>
                 </div>
@@ -93,35 +123,142 @@
             <div class="modal-body">
                 <div class="bg-dark">
                     <div class="row pt-1 text-center text-white-50">
-                        <div class="col">
+                        <div class="col-6">
                             <h6>Empresa o sector</h6>
                         </div>
-                        <div class="col">
-                            <h6>Duracion</h6>
-                        </div>
-                        <div class="col">
-                            <h6>Titulo</h6>
+                        <div class="col-6">
+                            <h6>Duración</h6>
                         </div>
                     </div>
-                    <div class="row pt-1 text-center text-white-50">
-                        <div class="col">
-                            <h6>Proceso</h6>
-                        </div>
-                        <div class="col">
-                            <h6>Tipología</h6>
-                        </div>
-                        <div class="col">
-                            <button type="button" class="mb-1 btn btn-warning btn-sm">
-                                <svg class="" width="20px" height="20px" fill="currentColor">
-                                    <use xlink:href="../svg/bootstrap-icons.svg#eye-fill" />
-                                </svg>
-                            </button>
-                        </div>
+                </div>
+                <div class="row d-flex align-items-center border-bottom mx-1">
+                    <div class="col px-0">
+                        <p class="mb-1">Juntas cantonales de protección de derechos empresa</p>
+                    </div>
+                    <div class="col-auto px-2">
+                        <p class="mb-1">2020-07-06-<br>2020-12-31</p>
+                    </div>
+                    <div class="col-auto px-0">
+                        <button type="button" class=" btn pt-0 pb-1 btn-warning btn-sm">
+                            <svg class="" width="20px" height="20px" fill="currentColor">
+                                <use xlink:href="../svg/bootstrap-icons.svg#file-earmark-post" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div class="row d-flex align-items-center border-bottom mx-1">
+                    <div class="col px-0">
+                        <p class="mb-1">Universidad Técnica de Manabí empresa</p>
+                    </div>
+                    <div class="col-auto px-2">
+                        <p class="mb-1">2020-07-06-<br>2020-12-31</p>
+                    </div>
+                    <div class="col-auto px-0">
+                        <button type="button" class=" btn pt-0 pb-1 btn-warning btn-sm">
+                            <svg class="" width="20px" height="20px" fill="currentColor">
+                                <use xlink:href="../svg/bootstrap-icons.svg#file-earmark-post" />
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </div>
             <div class="modal-footer py-0">
                 <button type="button" class="btn-sm btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Tarjeta más detalles-->
+<div class="modal fade" id="target-detalles">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header py-2 bg-primary">
+                <h6 class="modal-title text-white">Detalles del proyecto</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p><b>Creado por:</b> Zambrano Solorzano Elba Tatiana</p>
+                <p><b>Fecha de creación: </b> 2020-09-02 15:58:17</p>
+                <h6 class="font-weight-bold">Capacidad de inscripción</h6>
+                <p>Capacidad: 17 <br>
+                    Inscritos: 14 <br>
+                    Nivel Minimo: 10 <br>
+                    Nivel Maximo: 10
+                </p>
+                <h6 class="font-weight-bold">Duración del proyecto</h6>
+                <p>Del 2020-09-21 hasta 2021-03-31 <br> <b>80 horas</b></p>
+            </div>
+            <div class="modal-footer py-0">
+                <button type="button" class="btn-sm btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Tarjeta de atención-->
+<div class="modal fade" id="target-atencion">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header py-2 bg-primary">
+                <h6 class="modal-title text-white">Atención brindada</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="bg-dark">
+                    <div class="row pt-1 text-center text-white-50">
+                        <div class="col">
+                            <h6>Responsable</h6>
+                        </div>
+                        <div class="col-auto">
+                            <h6 class="mr-3">Atención</h6>
+                        </div>
+                    </div>
+                </div>
+                <ul class="list-group  ">
+                    <li class="list-group-item py-1 ">
+                        <div class="row d-flex align-items-center">
+                            <div class="col pr-1">
+                                <h5 class="mb-1 font-weight-bold text-small">Zambrano Montenegro David Fernando</h5>
+                                <p class="mb-1"><b>Observación:</b> Aprobado sin novedad</p>
+                                <small> <b>Fecha: </b>2021-07-05 16:00:30</small>
+                            </div>
+                            <div class="col-auto px-1">
+                                <span class="badge badge-pill badge-success">Aprobado</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="modal-footer py-0">
+                <button type="button" class="btn-sm btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Tarjeta de inscripción-->
+<div class="modal fade" id="target-inscripcion">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header py-2 bg-primary">
+                <h6 class="modal-title text-white">Inscripción</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h5>¿Desea inscribirse en este proyecto?</h5>
+            </div>
+            <div class="modal-footer py-0">
+                <button type="button" class="btn-sm btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn-sm btn-primary">
+                    <svg width="15px" height="15px">
+                        <use xlink:href="../svg/bootstrap-icons.svg#pencil-square" />
+                    </svg>
+                    Aceptar
+                </button>
             </div>
         </div>
     </div>

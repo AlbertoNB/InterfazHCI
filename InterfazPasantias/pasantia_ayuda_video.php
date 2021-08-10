2914ddf740f926ffa8e0d57a1febf59207ea44f7
Alberto Naula Bone <anaula8277@utm.edu.ec>
2021-08-10 00:44:28 -0500

    se agrego la interfaz de pasantia_ayuda_video.php

--- InterfazPasantias/pasantia_ayuda_video.php
+++ InterfazPasantias/pasantia_ayuda_video.php
@@ -1,60 +1,76 @@
 <div class="collapse" id="ayuda-video-p" data-parent="#pasantia">
-<div class="bg-secondary p-2 mb-2">
+    <div class="bg-secondary p-2 mb-2">
         <h5 class="text-white d-flex align-items-center text-small m-0">
-            Video pasantia
+            Video tutorial - ayuda para estudiantes - pasantias
         </h5>
     </div>
-    
-<!--
+
     <div class="mx-3 mode-dark2">
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            01. Acceder al sistema.
+            01. Acceder al sistema. 
         </p>
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            02. Seleccionar Módulo.
+            02. Enviar solicitud de pasantía.
         </p>
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            03. Inscripción - Consultar Información.
+            03. Imprimir documentos de la pasantía. 
         </p>
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            04. Inscripción - Registrar Inscripción.
+            04. Editar información de la pasantía.
         </p>
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            05. Inscripción - Borrar Inscripción.
+            05. Enviar Informe Mensual.
         </p>
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            06. Gestor - Consultar Información.
+            06. Editar Informe Mensual.
         </p>
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            07. Gestor - Consultar Asistencia.
+            07. Eliminar Informe Mensual.
         </p>
         <p>
             <svg class="text-danger" width="30px" height="30px">
                 <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
             </svg>
-            08. Verificar Certificado.
+            08. Imprimir Informe Mensual.
+        </p>
+        <p>
+            <svg class="text-danger" width="30px" height="30px">
+                <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
+            </svg>
+            09. Registrar Autoevaluación de la pasantía.
+        </p>
+        <p>
+            <svg class="text-danger" width="30px" height="30px">
+                <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
+            </svg>
+            10. Registrar Informe Final PDF.
+        </p>
+        <p>
+            <svg class="text-danger" width="30px" height="30px">
+                <use xlink:href="../svg/bootstrap-icons.svg#youtube" />
+            </svg>
+            11. Imprimir Certificado Final.
         </p>
     </div>
--->
 </div>
